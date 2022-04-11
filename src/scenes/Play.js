4ad9bd2fd03733dc1a0a7cc27a2bfc56738d6e4b
rasureiden <amhou@ucsc.edu>
2022-04-11 04:48:57 -0700

    uh some asseys, starfield scroll

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,7 +3,16 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
+    preload() {
+        // load images/tile sprites
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+    
     create() {
+        // place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         // green UI bg
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // white borders
@@ -11,5 +20,9 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+    }
+
+    update() {
+        this.starfield.tilePositionX -= 4;
     }
 }
