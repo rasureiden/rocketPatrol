ecbd8f975ccecdc26848adcf329966de45a7505b
rasureiden <amhou@ucsc.edu>
2022-04-15 17:58:08 -0700

    hopefully fixed??

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -25,7 +25,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.isFiring = true;
             this.sfxRocket.play();  // play rocket sfx
         }
-
+        
         // if fired, move up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
             this.y -= this.moveSpeed;
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -25,7 +25,7 @@ class Menu extends Phaser.Scene {
             fixedWidth: 0
         }
 
-        // show menu ttext
+        // show menu text
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
